75c938904821e3ef378a81261b7b50cd21f2af49
PresenceWith <sleet0328@gmail.com>
2025-12-31 16:00:24 +0900

    [chore] 초기 프로젝트 세팅

---
+++ refomo/refomoApp.swift
@@ -0,0 +1,17 @@
+//
+//  refomoApp.swift
+//  refomo
+//
+//  Created by Presence on 12/31/25.
+//
+
+import SwiftUI
+
+@main
+struct refomoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ refomoTests/refomoTests.swift
@@ -0,0 +1,17 @@
+//
+//  refomoTests.swift
+//  refomoTests
+//
+//  Created by Presence on 12/31/25.
+//
+
+import Testing
+@testable import refomo
+
+struct refomoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
